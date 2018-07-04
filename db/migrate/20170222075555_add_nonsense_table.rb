59dc0e41cf6c34ef08bfe83a3e0bb8dc2ccb18c5
Radamanthus Batnag <radamanthus@gmail.com>
2018-07-04 23:03:48 +0800

    Deleted the DelayedJob migrations

--- db/migrate/20170222075555_add_nonsense_table.rb
+++
@@ -1,9 +0,0 @@
-class AddNonsenseTable < ActiveRecord::Migration[5.0]
-  def change
-    create_table :nonsense do |t|
-      t.string :name
-      t.string :description
-      t.timestamps
-    end
-  end
-end
